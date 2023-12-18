7fe7b9ec06fb44c8b87d0a53979c666052bb331e
zhiyan114 <zhiyan114@zhiyan114.com>
2023-12-18 14:28:27 -0500

    sync the sames

--- prisma/postgre.sql
+++ prisma/postgre.sql
@@ -5,5 +5,5 @@ CREATE TABLE sensor_records (
     /* We won't use default value since this data must come from the sensor for accuracy (otherwise the time would offset by network and db query latency) */
     created_at TIMESTAMP NOT NULL,
     /* this is used for internal reference in-case multiple locations are involved */
-    location_name TEXT
+    device_name TEXT
 );
