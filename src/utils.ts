bd4c6e2cdb1cc91659facdd46cdcc6d3abfe9561
zhiyan114 <zhiyan114@zhiyan114.com>
2023-12-19 21:19:46 -0500

    Added env stuff for staging test

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import { existsSync } from "fs";
+import { execSync } from "child_process";
 /* Load up the commit hash */
-const commitHash = process.env["RAILWAY_GIT_COMMIT_SHA"];
+const commitHash = process.env["RAILWAY_GIT_COMMIT_SHA"] ??
+  existsSync(".git") ? execSync("git rev-parse HEAD").toString().trim() : undefined;
 if(commitHash) console.log(`Loading Software Version: ${commitHash}`);
 
 /* Load Prisma Client */
