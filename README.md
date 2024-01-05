bc44e6da3f8c4018692ac42a1f48d400d8a860eb
zhiyan114 <zhiyan114@zhiyan114.com>
2024-01-05 15:53:03 -0500

    Inital Stuff

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SensorServer
 Server is used to record sensor data from the MQTT topic
+## Difference?
+This is a C/C++ Implementation of the previous server software. Why?
+I simply want to cut down the cost on memory so there's that.
+
+### Cost Table
+
+
+Lang | Baseline | NodeJS | C/C++
+--- | --- | --- | ---
+Memory Usage | 1GB | 45-80 MB | ???
+Cost | $5/mo | $0.225-$0.4/mo | ????
--- prisma/postgre.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE sensor_records (
-    id SERIAL PRIMARY KEY,
-    temperature NUMERIC(5,2) NOT NULL,
-    humidity NUMERIC(5,2) NOT NULL,
-    /* We won't use default value since this data must come from the sensor for accuracy (otherwise the time would offset by network and db query latency) */
-    created_at TIMESTAMP UNIQUE NOT NULL,
-    /* this is used for internal reference in-case multiple locations are involved */
-    device_name TEXT
-);
