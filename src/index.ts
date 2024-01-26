efb8dbc8c83b8fba5219729f529f014078863993
zhiyan114 <zhiyan114@zhiyan114.com>
2024-01-25 23:32:52 -0500

    Sentry: ignore prisma timeout error

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { ExtraErrorData } from "@sentry/integrations";
 import { commitHash, PrismaCli } from "./utils";
 import './mqtt';
 import './cron';
+import { Prisma } from "@prisma/client";
 
 // Exit if node version isnt 18.XX+
 const nodeVersion = Number(process.version.split(".")[0].slice(1));
@@ -36,8 +37,12 @@ if(DSN) {
       "ECONNRESET",
     ],
     beforeSend: (event, hint) => {
+      // Filter out prisma timeout error
+      if(hint.originalException instanceof Prisma.PrismaClientKnownRequestError &&
+         hint.originalException.code == "P2024"
+      ) return null;
       // Only passing exceptions
-      if (hint?.originalException) return event;
+      if (hint?.originalException || (event.exception?.values?.length ?? 0) > 0) return event;
       // Or if there's error message
       if (event.level == "error") return event;
       return null;
